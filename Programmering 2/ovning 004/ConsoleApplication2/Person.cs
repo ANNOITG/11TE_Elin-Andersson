614bea4e6386759c3b9fa4e91767f50f442c3f8f
lynixnii <elin_a_95@hotmail.com>
2013-09-24 11:44:05 +0200

    Lade till grunderna i projektet
    Skapade klasserna Person -> Elev, Lärare. Med mera...

---
+++ Programmering 2/ovning 004/ConsoleApplication2/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Person
+    {
+        private string namn;
+        private string pnr;
+        private string telenr;
+        private string adress;
+
+        public string getNamn() { return namn; }
+        public void setNamn(string namn) { this.namn = namn; }
+
+        public string getPnr() { return pnr; }
+        public void setPnr(string personnummer) { this.pnr = personnummer; }
+
+        public string getTeleNr() { return telenr; }
+        public void setTeleNr(string telenr) { this.telenr = telenr; }
+
+        public string getAdress() { return adress; }
+        public void setAdress(string adress) { this.adress = adress; }
+    }
+}
